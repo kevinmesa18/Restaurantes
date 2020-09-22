d6ecd9f002861e7cba357399a446f7730663d95a
Kevin Mesa <kevinmesa18@gmail.com>
2020-09-21 21:13:52 -0500

    Ajuste vista home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-6">
             <div class="text-center text-white">
-                <img class="card-img-top rounded-circle" alt="Restaurantes" src="https://cnnespanol.cnn.com/wp-content/uploads/2019/01/Galerias-nuevos-restaurantes-2019-CNN-1.jpg?quality=100&strip=info&w=320&h=240&crop=1" >
+                <img class="rounded-circle" width="300" height="300" alt="Restaurantes" src="https://cnnespanol.cnn.com/wp-content/uploads/2019/01/Galerias-nuevos-restaurantes-2019-CNN-1.jpg?quality=100&strip=info&w=320&h=240&crop=1" >
                 <div class="card-body">
                     <p class="card-text">Conoce los mejores restaurantes en tu ciudad</p>
                     <a href="/restaurantes" class="btn btn-primary">Ver listado de restaurantes</a>
@@ -16,7 +16,7 @@
         </div>
         <div class="col-6">
             <div class="text-center text-white">
-                <img class="card-img-top rounded-circle" alt="Reservas" src="https://i.pinimg.com/originals/ae/fb/df/aefbdf40cd5b178aeaa3d16830404d28.jpg">
+                <img class="rounded-circle" width="300" height="300" alt="Reservas" src="https://i.pinimg.com/originals/ae/fb/df/aefbdf40cd5b178aeaa3d16830404d28.jpg">
                 <div class="card-body">
                     <p class="card-text">Pide tu mesa en los mejores restaurantes de la ciudad</p>
                     <a href="/reservas" class="btn btn-primary">Pedir reservar</a>
